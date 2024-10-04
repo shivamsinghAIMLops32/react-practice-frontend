166f5bfe35141807a8cf4b9804d69bf84ec67d38
shivamsinghAIMLops32 <136737397+shivamsinghAIMLops32@users.noreply.github.com>
2024-10-04 19:33:35 +0530

    props.children

--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,19 @@
-import React from "react";
-import Person from "./components/Person";
-import Product from './components/Product';
+import React from 'react';
+import Container from './components/Container';
 
 const App = () => {
-  const name = "shivam";
-  const age = 18;
-
-  const products = [
-    { pid: 1, productName: "lenovo", price: 17000 },
-    { pid: 2, productName: "asus", price: 29000 }
-  ];
-
   return (
     <div>
-      <Person name={name} age={age} />
-      {products.map((prod) => (
-        <Product key={prod.pid} product={prod} />
-      ))}
+      <Container>
+        <p>This is some content inside the container.</p>
+        <button>Click Me!</button>
+      </Container>
+
+
+      <Container>
+        <h3>Another container with different content!</h3>
+        <button>submit</button>
+      </Container>
     </div>
   );
 };
