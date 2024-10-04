730dce23e7f676f88d6726a5744ed0882f969e08
shivamsinghAIMLops32 <136737397+shivamsinghAIMLops32@users.noreply.github.com>
2024-10-04 17:51:14 +0530

    array iterate 1st

--- src/App.jsx
+++ src/App.jsx
@@ -1,28 +1,25 @@
-import React, { useState } from 'react';
-import Greeting from './components/Greeting';
-import ProductInfo from './components/ProductInfo';
+import React, { useState } from "react";
+
 
 const App = () => {
-  const [name, setName] = useState("");
   
-  const [date,setDate]= useState("");
-  const handleInputChange = (e) => {
-    setName(e.target.value); // Update state with input value
-    setDate(new Date().toLocaleString())
-  };
-
-  
+  const number = [1, 2, 3, 7, ,9,4];
   return (
     <div>
-      <input
-        type="text"
-        required
-        placeholder="name"
-        value={name}
-        onChange={handleInputChange}
-      />
-      <Greeting name={name} date={date} />
-      <ProductInfo/>
+      <main>
+        <ul>
+          {number.map((num) => {
+            const randomColor = `#${Math.floor(
+              Math.random() * 16777215
+            ).toString(16)}`;
+            return (
+              <li key={num} style={{ color: randomColor }}>
+                {num}
+              </li>
+            );
+          })}
+        </ul>
+      </main>
     </div>
   );
 };
